d605a610bc4606356b7853f81648e0ba307be93b
FormalHellhound <FormalHellhound@gmail.com>
2016-06-03 16:51:09 -0400

    Fixed a few small issues preventing the api from working properly.

--- lib/pastebin-api.rb
+++ lib/pastebin-api.rb
@@ -12,13 +12,13 @@ module Pastebin
  
       auth[:api_dev_key] = @api_key
 
-      if auth['api_user_name'].nil?
+      if !auth[:api_user_name].nil?
  
-        @userkey = ''
+        @userkey = RestClient.post('http://pastebin.com/api/api_login.php', auth).body
 
       else
  
-        @userkey = RestClient.post('http://pastebin.com/api/api_login.php', auth).body
+        @userkey = ''
 
       end
 
@@ -26,8 +26,10 @@ module Pastebin
 
     def newpaste(message, params = {})
 
-      params = {api_dev_key: @api_key, api_user_key: @userkey, api_option: 'paste', api_paste_code: message, api_paste_name: '',
+      params = {api_dev_key: @api_key, api_user_key: '', api_option: 'paste', api_paste_code: message, api_paste_name: '',
                 api_paste_format: 'text', api_paste_private: '0', api_paste_expire_date: 'N'}.merge(params)
+
+      params[:api_user_key] = @userkey
 
       response = RestClient.post('http://pastebin.com/api/api_post.php', params).body
 
--- pastebin-api.gemspec
+++ pastebin-api.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'pastebin-api'
-  s.version     = '1.0.4'
+  s.version     = '1.0.5'
   s.date        = '2016-06-03'
   s.summary     = "Pastebin API Wrapper"
   s.description = "A wrapper for accessing the pastebin API."
